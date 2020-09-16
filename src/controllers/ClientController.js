75cd21d35513e19b94ae8b3a05114750616255fe
juliano-rb <rocha.juliano.b@gmail.com>
2020-09-15 22:01:57 -0300

    Adiciona a possibilidade de filtrar cliente ao listar

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -4,7 +4,9 @@ const Client = require('../models/Client.js');
 const ClientController = {
   async list(req, res) {
     const repo = getRepository(Client);
-    const client = await repo.find();
+    // filtra os clientes se passar um filtro no body
+    const client = await repo.find(req.body);
+
     return res.json(client);
   },
   async add(req, res) {
