d83f4b10766c92d7c5d0f19c41e7455da45c0dd6
juliano-rb <rocha.juliano.b@gmail.com>
2020-10-03 22:39:57 -0300

    Fixing vue 3 upgrade erros

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import Clients from "./components/Clients.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-Vue.config.productionTip = false
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: "/", component: Clients }],
+});
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.mount("#app");
