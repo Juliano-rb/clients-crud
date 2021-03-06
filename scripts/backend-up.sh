ffa9105abc752d4df54aeb8880f8e40127cc776a
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2021-03-06 14:21:14 -0300

    remove scripts referentes a antigo container docker mssql

--- scripts/backend-up.sh
+++
@@ -1,4 +0,0 @@
-echo "## Configurando requisitos e iniciando backend                ##"
-echo "* Caso algum script apresente erro, favor executar novamente *\n"
-
-./setupcontainer.sh && ./createdb.sh && npm start
